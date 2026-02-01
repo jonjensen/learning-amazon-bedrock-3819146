9d6160d86d46f21fd5ce0b673315d53d13616197
Jon Jensen <jon@swelter.net>
2026-02-01 04:06:34 +0000

    Flesh out demos based on training

--- 02_03b/boto3-bedrock-sample.py
+++ 02_03b/boto3-bedrock-sample.py
@@ -1,13 +1,40 @@
 #Imports
+import boto3
+from botocore.exceptions import ClientError
 
 #Create the bedrock client
+client = boto3.client('bedrock-runtime')
 
 #Setting the prompt
+user_message = """Command: Write me a blog post about coaching employees as a leader.
+
+Blog:
+"""
 
 #Model specification
+model_id = "amazon.nova-lite-v1:0"
+accept = "application/json"
+contentType = accept
 
-#Configuring parameters to invoke the model
+conversation = [
+    {
+        "role": "user",
+        "content": [{"text": user_message}],
+    }
+]
 
-#Invoke the model
+try:
+    # Send the message to the model, using a basic inference configuration.
+    response = client.converse(
+        modelId=model_id,
+        messages=conversation,
+        inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9},
+    )
 
-#Parsing and displaying the output
+    # Extract and print the response text.
+    response_text = response["output"]["message"]["content"][0]["text"]
+    print(response_text)
+
+except (ClientError, Exception) as e:
+    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+    exit(1)
